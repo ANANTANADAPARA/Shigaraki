576ac27f5732f36944223b40d2f252b32161ebaf
ANANTANADAPARA <131572814+ANANTANADAPARA@users.noreply.github.com>
2024-06-08 13:08:46 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,7 @@
  pip install subprocess
  pip install re
  pip install pyfiglet
+
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/halive/cmd/halive@latest
